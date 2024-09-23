05391a210e0d9d0f7467a3bab7241d18ca001cfe
naufalF21 <naufalfirman098@gmail.com>
2024-09-24 00:46:53 +0700

    style: change gap of project Student page in mobile view

--- resources/views/pages/student/sections/projects.blade.php
+++ resources/views/pages/student/sections/projects.blade.php
@@ -1,5 +1,5 @@
 <section class="container pb-14 md:py-14">
-    <ul class="grid md:grid-cols-4 grid-cols-2 gap-10 w-full">
+    <ul class="grid md:grid-cols-4 grid-cols-2 md:gap-10 gap-5 w-full">
         @foreach ($unfavoriteVideo as $item)
             <li>
                 <iframe class="w-full h-[250px] rounded-xl" src="{{ $item->link }}" frameborder="0" loading="lazy"
