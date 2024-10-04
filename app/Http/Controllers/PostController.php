9d28aa9eb9f51d4cc95fe3e33196540c2dc65e90
naufalF21 <naufalfirman098@gmail.com>
2024-10-05 06:28:58 +0700

    feat: setup cache for Categories

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,17 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
 
 class PostController extends Controller
 {
     public function index()
     {
+        $categories = Cache::remember('categories', now()->addDays(3), function () {
+            return Category::whereHas('posts', function ($query) {
+                $query->published();
+            })->take(10)->get();
+        });
+
         return view(
             'pages.articles.index',
             [
-                'categories' => Category::whereHas('posts', function ($query) {
-                    $query->published();
-                })->take(10)->get()
+                'categories' => $categories
             ]
         );
     }
